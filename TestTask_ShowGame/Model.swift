d8adb49034bcdaf010605caab681bb3b25bda91c
kneskoromny <kneskoromny@gmail.com>
2021-12-08 15:27:13 +0300

    модель

---
+++ TestTask_ShowGame/Model.swift
@@ -0,0 +1,35 @@
+//
+//  Model.swift
+//  TestTask_ShowGame
+//
+//  Created by Кирилл Нескоромный on 08.12.2021.
+//
+
+import Foundation
+
+// MARK: - Game
+struct Game: Codable {
+    let tournament: Info
+    let date: String
+    let team1, team2: Info
+    let streamStatus: Int
+
+    enum CodingKeys: String, CodingKey {
+        case tournament, date, team1, team2
+        case streamStatus = "stream_status"
+    }
+}
+
+// MARK: - Info
+struct Info: Codable {
+    let id: Int
+    let nameEng, nameRus: String
+    let score: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case nameEng = "name_eng"
+        case nameRus = "name_rus"
+        case score
+    }
+}
