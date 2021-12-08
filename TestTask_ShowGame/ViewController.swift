53d72a8fe07f4d7202f951e47181fb967b4cb755
kneskoromny <kneskoromny@gmail.com>
2021-12-08 14:40:42 +0300

    Initial Commit

---
+++ TestTask_ShowGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask_ShowGame
+//
+//  Created by Кирилл Нескоромный on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
